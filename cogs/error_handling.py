a063f2aa573c539ccdd21728945792eb7d660886
magicdevm <aaronfernan78@gmail.com>
2025-08-15 14:02:52 +0530

    feat[error_handling: Added more types of error catching

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -12,6 +12,14 @@ class ErrorHandling(commands.Cog):
                 await ctx.send("You don't have permission to use this command.")
             elif isinstance(error, commands.CommandNotFound):
                 return
+            elif isinstance(error, commands.BadArgument):
+                await ctx.send("One of your arguments is invalid.")
+            elif isinstance(error, commands.MissingPermissions):
+                await ctx.send("You don’t have permission to use this command.")
+            elif isinstance(error, commands.BotMissingPermissions):
+                await ctx.send("I don’t have permission to do that.")
+            elif isinstance(error, commands.NoPrivateMessage):
+                await ctx.send("You can’t use this command in DMs.")
             else:
                 print(f"Unexpected error: {error}")
                 error = discord.Embed(title="An unexpected error occured", description=f"An unexpected error occurred while running `{ctx.command}`\n\n**ERROR**: __{error}__", colour=discord.Colour.red())
