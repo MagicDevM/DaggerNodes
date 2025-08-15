291e7bb20342e94bb4d727609b54583d9672e30c
magicdevm <aaronfernan78@gmail.com>
2025-08-15 11:37:27 +0530

    refactor[naming]: Changed bot to client refactor[description]: updated command description

--- cogs/general.py
+++ cogs/general.py
@@ -2,17 +2,17 @@ import discord
 from discord.ext import commands
 
 class General(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, client):
+        self.client = client
 
-    @commands.hybrid_command(name="ping", description="Check bot latency.")
+    @commands.hybrid_command(name="ping", description="Check the bot's latency.")
     async def ping(self, ctx):
-        await ctx.send(f'Pong! **{round(self.bot.latency * 1000)}ms**')
+        await ctx.send(f'Pong! **{round(self.client.latency * 1000)}ms**')
     @commands.hybrid_command(name="help", description="help command.")
     async def help(self, ctx):
         help = discord.Embed(title="List of all commands", description="Supported types: **message** & **application commands**\n\n__General__:\ndn!help\ndn!ping\n\n__Economy__:\ndn!bal\ndn!add (admin only)\ndn!set (admin only)", colour=discord.Colour.orange())
         help.set_footer(text=f"requested by {ctx.author.name}", icon_url=ctx.author.display_avatar.url)
         await ctx.send(embed=help)
 
-async def setup(bot):
-    await bot.add_cog(General(bot))
+async def setup(client):
+    await client.add_cog(General(client))
