7baf7753e5eb17f3d94da5761403e3c3bc96a258
magicdevm <aaronfernan78@gmail.com>
2025-08-15 12:04:45 +0530

    fix[db]: UserNotFound bug fixed in hybrid command

--- cogs/economy.py
+++ cogs/economy.py
@@ -8,12 +8,19 @@ class Economy(commands.Cog):
     @commands.hybrid_command(name="balance", description="Check you're balance.")
     async def balance(self, ctx):
         cursor = self.client.cursor
+        conn = self.client.conn
         cursor.execute('SELECT balance FROM economy WHERE user_id = ?', (ctx.author.id,))
         results = cursor.fetchone()
-        balance = results[0] if results else 0
+        if results is None:
+            cursor.execute("INSERT INTO economy (user_id, balance) VALUES (?, ?)", (ctx.author.id, 0))
+            conn.commit()
+            balance = 0
+        else:
+            balance = results[0]
         bal = discord.Embed(title="Balance", description=f"__Balance__: **${balance}**", colour=discord.Colour.green())
         bal.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.display_avatar.url)
         await ctx.send(embed=bal)
+    
     @commands.command(name="bal", description="Check you're balance.")
     async def bal(self, ctx):
         cursor = self.client.cursor
